6f4843f1be5b14a7836fb95c42874987fa163d9f
VeronicaFontana <fontana.veronica98@gmail.com>
2023-11-19 23:01:49 +0100

    [ADD] Icons

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <script src="https://kit.fontawesome.com/66b933bf5d.js" crossorigin="anonymous"></script>
     @vite('resources/js/app.js')
 
     <title>Document</title>
