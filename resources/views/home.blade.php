e00f9c5c411d5c95c1a3d5e96fd52e3971c0bf7d
VeronicaFontana <fontana.veronica98@gmail.com>
2023-11-19 20:41:35 +0100

    [ADD] SCSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,45 @@
 
     <section class="container">
 
-        @include("partials.card")
+        @foreach ($products as $product)
+            <div class="card">
+                <div class="image-box">
+                    <img class="first-img" src="{{ $product["frontImage"] }}" alt="">
+                    <img class="second-img" src="{{ $product["backImage"] }}" alt="">
 
-        <!-- <Card v-for="(prodotto, index) in prodotti" :key="index" :productObj="prodotto" /> -->
+                    @if(isset($product["badges"]))
+                        @foreach($product["badges"] as $badge)
+                            @if($badge["type"] === "discount" && $badge["type"] === "tag")
+                                <span class="discount">
+                                    {{ $badge["value"] }}
+                                </span>
+                                <span class="sustain">
+                                    Sostenibilità
+                                </span>
+                            @elseif($badge["type"] === "discount")
+                                <span class="discount">
+                                    {{ $badge["value"] }}
+                                </span>
+                            @elseif($badge["type"] === "tag")
+                                <span class="sustain">
+                                    Sostenibilità
+                                </span>
+                            @endif
+                        @endforeach
+                    @endif
+
+                    <span class="heart">&hearts;</span>
+                </div>
+                <div class="text-box">
+                    <span class="brand">{{ $product["brand"] }}</span>
+                    <h4>{{ $product["name"] }}</h4>
+                    <div class="price">
+                        <span >{{ $product["price"] }} &euro;</span>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
     </section>
 
 @endsection
